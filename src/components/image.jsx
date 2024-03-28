fdbf95e073452d36456e03f8d3e9adc9bea99530
Asadbek <91201659+AsadbekMuydinov98@users.noreply.github.com>
2024-03-29 06:57:47 +0900

    Update image.jsx

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,8 +1,9 @@
 export default function Image({src,...rest}) {
   src = src && src.includes('https://')
     ? src
-    : 'http://localhost:5000/uploads/'+src;
+    : 'https://airbnb-clone-backend-2y3a.onrender.com/uploads/'+src;
   return (
     <img {...rest} src={src} alt={''} />
   );
 }
+// 'http://localhost:5000/uploads/'
